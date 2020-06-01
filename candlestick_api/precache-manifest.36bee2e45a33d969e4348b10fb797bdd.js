7e62a7a8e50c0eb1060fb6e29f955a197cca56a1
e81u4g <e81u4g@gmail.com>
2020-06-01 21:57:55 +0800

    test

---
+++ candlestick_api/precache-manifest.36bee2e45a33d969e4348b10fb797bdd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "117f86ee2268f8afa038abceb478c9b1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5becf9190f00823d83db",
+    "url": "./static/css/2.42ec2a3e.chunk.css"
+  },
+  {
+    "revision": "568e66bd77c2a5a29f11",
+    "url": "./static/css/main.b96b9f6a.chunk.css"
+  },
+  {
+    "revision": "5becf9190f00823d83db",
+    "url": "./static/js/2.26360bc4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.26360bc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "568e66bd77c2a5a29f11",
+    "url": "./static/js/main.f7e9093c.chunk.js"
+  },
+  {
+    "revision": "a66a48eb3151ae4c61cd",
+    "url": "./static/js/runtime-main.449974e7.js"
+  }
+]);
